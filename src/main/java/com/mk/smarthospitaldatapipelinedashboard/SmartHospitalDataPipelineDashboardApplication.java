c92080f1821d422b01093d877aee51a1bef8edba
manoj-kumar-006 <manojkumar5621r@gmail.com>
2026-03-12 15:06:19 +0530

    Initial commit

---
+++ src/main/java/com/mk/smarthospitaldatapipelinedashboard/SmartHospitalDataPipelineDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.mk.smarthospitaldatapipelinedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHospitalDataPipelineDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHospitalDataPipelineDashboardApplication.class, args);
+    }
+
+}
